db6511d50272f60e7034c567cea4c015734497c1
shanecfield1 <shanecaulfield101@gmail.com>
2025-11-14 13:01:38 +0000

    Update ShaneCaulfieldLibrary.java

--- ShaneCaulfieldLibrary.java
+++ ShaneCaulfieldLibrary.java
@@ -1,12 +1,27 @@
 //Insert package name
 
 //Rename class using Student's Name
-public class ShanCaulfieldLibrary {
+public class ShaneCaulfieldLibrary {
 
     //Insert student 3's function's below
 
+    import java.util.HashMap;
 
+public class Main {
+  public static void main(String[] args) {
+    String text = "banana";
+    HashMap<Character, Integer> freq = new HashMap<>();
 
+    for (char c : text.toCharArray()) {
+      freq.put(c, freq.getOrDefault(c, 0) + 1);
+    }
+
+    System.out.println(freq);
+    // Output: {a=3, b=1, n=2}
+  }
+}
+
+    
     
    //Example function 
     public static int getLength(String str) {
