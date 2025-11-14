df879e8c7aa01ed469067127eff9bd2dbd5b0b20
Jon40K <2025jomurphy@loetbstudent.ie>
2025-11-14 13:02:32 +0000

    Update TeamStringProgram.java
    Put the code for the first version of the main menu

--- TeamStringProgram.java
+++ TeamStringProgram.java
@@ -1,52 +1,60 @@
 //Insert Relevant Package Name
+import static JonathanMurphyLibrary.ReverseWords;
+import java.util.Scanner;
+
 
 public class TeamStringProgram {
+    public static Scanner userInput = new Scanner( System.in);
+    public static String userString;
+    public static String chosenOption;
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-
-      //Decalre and initialise variables 
-        Scanner scanner = new Scanner(System.in);
-
-        boolean running = true;
-
-      //Output program heading
-
-
-      //Loop to repeat program
-
-            //Prompt user for relevant info
+        // TODO code application logic here
+        System.out.println("Welcome, \n Please enter a string to be analysed.");
+        do {
+            System.out.println("Enter string here: ");
+            userString = userInput.nextLine();
+        } while (userString == null);
+        
+        System.out.println("Enter the number of the operation you wish to carry out on the string.");
+        System.out.println("1. Count Vowels \n2. Reverse the String \n3. Convert String to uppercase \n4. Generate an acronym \n5. Count the number of words \n6. Count the number of characters \n7. count the number of digits \n8. Reverse each word in the string \n9. Check if the string is a palindrome");
+        System.out.println("Enter string here: ");
+        chosenOption = userInput.nextLine();
+        switch (chosenOption) {
+            case "1" -> {
+                //Function here
+            } 
+            case "2" -> {
+                //Function here
+            }
+            case "3" -> {
+                //Function here
+            }
+            case "4" -> {
+                //Function here
+            }
+            case "5" -> {
+                //Function here
+            }
+            case "6" -> {
+                //Function here
+            }
+            case "7" -> {
+                //Function here
+            }
+            case "8" -> {
+                ReverseWords(userString);
+            }
+            case "9" -> {
+                JonathanMurphyLibrary.PalindromeChecker(userString);
+            }
             
-            //Call a function to display the program menu
-
-            //Call a function to get the users menu choice
-            int choice = getChoice(scanner);
-
-            //Switch on the user's menu choice
-            switch (choice) {
-                case 1:
-                    //Call relevant method in relevant library and display result
-                    System.out.println("Length of the string: " + Student1NameLibrary.getLength(inputString));
-                    break;
- 
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            } //end switch 
-      
-        //end loop
-
-        scanner.close();
-
-        //Display exit message
-      
+        }
     }
-
-    private static void displayMenu() {
-
-       //Output menu options
-        System.out.println("\nChoose an option:");
-
-    }
-
   // Modify getChoice method as required
+    /*
     private static int getChoice(Scanner scanner) {
         int choice = -1;
         while (choice < 1 || choice > 3) {
@@ -59,4 +67,5 @@ public class TeamStringProgram {
         }
         return choice;
     } //end main 
+    */
 } //end class
