2b55ee70c652a6b99d07e4cfc0f5e6d0a5f0837a
Dandar <103852756+DandarGaming@users.noreply.github.com>
2025-11-14 13:12:01 +0000

    Implement CountWords class for word counting
    Added CountWords class to count words in a sentence.

--- DanielSukhanovLibrary.java
+++ DanielSukhanovLibrary.java
@@ -1,14 +1,21 @@
 //Insert package name
-
+import java.util.Scanner; // imports Scanner library
 //Rename class using Student's Name
 public class DanielSukhanovLibrary {
 
     //Insert student 1's function's below
+public class CountWords {
 
-   //Example function 
-    public static int getLength(String str) {
-        return str.length();
-    }
+       public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Declaration for Scanner
+        System.out.println("Please input your sentence");
+        String sentence = input.nextLine();
+       int Words = sentence.split("\\s").length;
+       System.out.println(Words);
+
+    }// end main
+    
+}//end class
 
    //Function 2
 
@@ -17,3 +24,5 @@ public class DanielSukhanovLibrary {
    
     
 } //end class 
+
+//TODO: CHANGE THE CLASSES INTO FUNCTIONS!!!!
