3058d4d3ecf654a3b5b89c6c31e0b1bde8ad3308
Dandar <103852756+DandarGaming@users.noreply.github.com>
2025-11-16 20:06:36 +0000

    Refactor CountWords and CountNum methods
    Refactored CountWords and CountNum methods to accept a string parameter instead of using main method for input. Fixed typo in replaceAll method.

--- DanielSukhanovLibrary.java
+++ DanielSukhanovLibrary.java
@@ -4,28 +4,24 @@ import java.util.Scanner; // imports Scanner library
 public class DanielSukhanovLibrary {
 
     //Insert student 1's function's below
-public class CountWords {
-
-       public static void main(String[] args) {
+public static void CountWords(String userStr) {
+        userStr = userStr.repalceAll(" ","");
         Scanner input = new Scanner(System.in); // Declaration for Scanner
         System.out.println("Please input your sentence");
         String sentence = input.nextLine();
        int Words = sentence.split("\\s").length;
        System.out.println(Words);
 
-    }// end main
     
 }//end class
 
    //Function 2
-public class CountNum {
-        
-    
-        public static void main(String[] args) {
+public static void CountNum(String userStr) {
+        userStr = userStr.replaceAll(" ", "");
         Scanner input = new Scanner(System.in); // Declaration for Scanner
         System.out.println("Please input your sentence");
         String sentence = input.nextLine();
-        int Numcount = 0;
+        int Numcount = 0;// sets the integer variable Numcount to zero 
 
         for (char i : sentence.toCharArray()) {//for loop to check through the character array of sentence variable
           if (Character.isDigit(i)) {
@@ -33,7 +29,6 @@ public class CountNum {
           }
         }
         System.out.println("Digits: " + Numcount);
-  }
 }
 
   //Function 3 (if applicable)
