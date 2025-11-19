631074ddb37d929e3d01aa3d3fb6dfeb4eb5e545
Dandar <103852756+DandarGaming@users.noreply.github.com>
2025-11-19 14:18:10 +0000

    Rename Example method to ConvertUpper
    Refactor Example method to ConvertUpper and adjust input handling.

--- ShaneCaulfieldLibrary.java
+++ ShaneCaulfieldLibrary.java
@@ -3,11 +3,11 @@ import java.util.HashMap;
 //Rename class using Student's Name
 public class ShaneCaulfieldLibrary {
     //Insert student 3's function's below
-  public static void Example(String[] args) {//change this into a function would look like this "public static void Example(Input 1,Input 2)"
-    String text = "banana"; // Remove this as we need to use the user input (userStr) See main java file
+  public static void ConvertUpper(String userStr) {
+   userStr = userStr.replaceAll(" ","");
     HashMap<Character, Integer> freq = new HashMap<>();
 
-    for (char c : text.toCharArray()) {
+    for (char c : userStr.toCharArray()) {
       freq.put(c, freq.getOrDefault(c, 0) + 1);
     }
 
