06078322ce9fc235944194675ee7e79318af9f40
DSMITH32123 <2025dsmith@loetbstudent.ie>
2025-11-20 16:37:46 +0000

    DylanSmithLibrary.java

--- DylanSmithLibrary.java
+++ DylanSmithLibrary.java
@@ -3,7 +3,28 @@
 //Rename class using Student's Name
 public class DylanSmithLibrary {
 
-    //Insert student 4's function's below
+    //generate acronym
+    public static String generateAcronym(String text) {
+        String[ ] words = text.split(" "); // splitting text ino individual words using ( ) to seperate
+        String result = ""; // empty string to store acronym 
+        String ignore = {"and", "of", "the", "an", "in", "on"}; // ignoring as it will add extra letter to acronym
+        for (String word : words) {  //loop through words
+            if (!word.isEmpty()) {  //skip extra space
+                boolean skip = false; // to check for ignored words 
+                for (String i : ignore) { //skip if word is ignored 
+                    if (word.equalsIgnoreCase(i)) {
+                        skip = true; //skipping word if matches ignore list
+                        break; // stopping check
+                    }
+                }
+                if (!skip) {
+                    result += Character.toUpperCase(word.charAt(0));
+                }
+            }
+        }
+        return result;
+    }
+}
 
    //Example function 
     public static int getLength(String str) {
